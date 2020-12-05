37a3d92c93d8890b4cfd22a681a902eb4bd60228
Jacob Prud'homme <jacobprudhomme@users.noreply.github.com>
2020-12-04 21:27:43 -0500

    Completed solution for Day 2 Part 1

---
+++ day2/part1.hs
@@ -0,0 +1,23 @@
+#!/usr/bin/env runhaskell
+
+type Line = (Int,Int,Char,String)
+
+parseLine :: String -> Line
+parseLine str = (read minStr, read maxStr, head letterStr, drop 2 rest3)
+  where
+    (minStr, rest1) = break (== '-') str
+    (maxStr, rest2) = break (== ' ') (tail rest1)
+    (letterStr, rest3) = break (== ':') (tail rest2)
+
+countOccurrences :: Line -> (Int,Int,Int)
+countOccurrences (minVal,maxVal,letter,password) = (minVal, maxVal, occurrences)
+  where occurrences = length $ filter (== letter) password
+
+isValid :: (Int,Int,Int) -> Bool
+isValid (minVal,maxVal,occurrences) = minVal <= occurrences && occurrences <= maxVal
+
+countValid :: [Line] -> Int
+countValid = length . filter id . map (isValid . countOccurrences)
+
+main :: IO ()
+main = interact $ show . countValid . map parseLine . lines
