a93eef76aeda13c8483048ff320aff80a3282a0b
Jacob Prud'homme <jacobprudhomme@users.noreply.github.com>
2020-12-27 01:39:05 -0500

    Completed solution for Day 21 Part 1

---
+++ day21/part1.hs
@@ -0,0 +1,40 @@
+#!/usr/bin/env runhaskell
+
+import Control.Arrow ((&&&))
+import Data.List.Split (splitOn)
+import Data.Map.Strict (Map)
+import Data.Set (Set, (\\))
+
+import qualified Data.Map.Strict as M
+import qualified Data.Set as S
+
+type Ingredient = String
+type Allergen = String
+
+-- Set intersection drops any ingredients that cannot contain the allergen, because the ingredient
+-- containing that allergen would have to be in every single food containing that allergen
+potentialIngredientsContainingAllergen :: [([Ingredient],[Allergen])] -> ([([Ingredient],[Allergen])], Map Allergen (Set Ingredient))
+potentialIngredientsContainingAllergen = id &&& (M.unionsWith S.intersection . map (uncurry insertAllergensForIngredients))
+  where
+    insertAllergensForIngredients ingredients allergens =
+      let ingredientsSet = S.fromList ingredients
+      in foldr (\allergen acc -> M.insert allergen ingredientsSet acc) M.empty allergens
+
+determineIngredientsContainingNoAllergens :: [([Ingredient],[Allergen])] -> Map Allergen (Set Ingredient) -> Set Ingredient
+determineIngredientsContainingNoAllergens ingredientsAndAllergens allergensToPotentialIngredients =
+  let allIngredients = S.unions (map (S.fromList . fst) ingredientsAndAllergens)
+      ingredientsPotentiallyContainingAllergen = S.unions (M.elems allergensToPotentialIngredients)
+  in allIngredients \\ ingredientsPotentiallyContainingAllergen
+
+countIngredientsContainingNoAllergens :: [([Ingredient],[Allergen])] -> Int
+countIngredientsContainingNoAllergens ingredientsAndAllergens =
+  let allIngredients = foldr (\(ingredients,_) acc -> ingredients ++ acc) [] ingredientsAndAllergens
+      ingredientsContainingNoAllergens = uncurry determineIngredientsContainingNoAllergens (potentialIngredientsContainingAllergen ingredientsAndAllergens)
+  in foldr (\ingredient acc -> length (filter (==ingredient) allIngredients) + acc) 0 ingredientsContainingNoAllergens
+
+splitIngredientsAndAllergens :: String -> ([Ingredient],[Allergen])
+splitIngredientsAndAllergens = stringsToLists . splitOn " (contains "
+  where stringsToLists [ingredients,allergens] = (words ingredients, splitOn ", " (init allergens))
+
+main :: IO ()
+main = interact $ show . countIngredientsContainingNoAllergens . map splitIngredientsAndAllergens . lines
